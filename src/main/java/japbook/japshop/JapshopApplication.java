53bc36392215285904fd29ac6a7cab3d79bcc6af
Shabet <pakjunyoung@nate.com>
2024-06-11 18:58:21 +0900

    lombok이 제대로 되는지 테스트 코드 추가

--- src/main/java/japbook/japshop/JapshopApplication.java
+++ src/main/java/japbook/japshop/JapshopApplication.java
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JapshopApplication {
 
 	public static void main(String[] args) {
+//		Hello hello = new Hello();
+//		hello.setData("Hello");
+//		String data = hello.getData();
+//		System.out.println("data = " + data);
+
 		SpringApplication.run(JapshopApplication.class, args);
 	}
 
